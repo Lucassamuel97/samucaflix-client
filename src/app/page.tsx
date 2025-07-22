051ca6ec674867fea2964e2713bd63f48cc9b1de
Lucassamuel97 <lukassamuka88@gmail.com>
2025-07-22 19:52:49 -0300

    :sparkles: feat: Add banner image and update Home component content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,32 @@
 import Image from 'next/image';
+import { Inter } from 'next/font/google';
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
-    <div className='relative h-full lg:h-[140vh]'>
-      <main className='relative pb-24 pl-4 lg:space-y-24 lg:pl-16'>
+    <div className='relative h-screen overflow-hidden bg-gradient-to-b lg:h-[140vh]'>
+      <main className='relative pb-24 pl-4 lg:pl-16'>
         <div className='flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12'>
-          <div className='absolute top-0 left-0 -z-10 flex h-[95vh] w-screen flex-col bg-black'>
+          <div className='absolute left-0 top-0 -z-10 flex h-[95vh] w-screen flex-col bg-black'>
             <Image
-              src='/banner5.jpg'
-              alt='The Witcher'
+              src='/banner.png'
+              alt='MAID'
               fill={true}
               className='h-[65vh] object-cover object-top lg:h-[95vh]'
             />
           </div>
-          <h1 className='text-2xl font-bold md:text-4xl lg:text-7xl'>
-            The Dictator
-          </h1>
-          <p className='max-w-xs text-xs text-shadow-md md:max-w-lg md:text-lg lg:max-w-2xl'>
-            Em visita à ONU em Nova York, um ditador acaba sequestrado e
-            barbeado. Sem seu visual característico, ele assiste ao tio traidor
-            assumir o poder, mas, com a ajuda de uma feminista vegetariana do
-            Brooklyn, tentará reconquistar seu trono.
+
+          <h1 className='text-2xl font-bold md:text-4xl lg:text-7xl'>MAID</h1>
+
+
+
+          <p className='text-shadow-md max-w-xs text-xs md:max-w-lg md:text-lg lg:max-w-2xl'>
+            After fleeing an abusive relationship, a young mother finds a job
+            cleaning houses as she fights to provide for her child and build
+            them a better future.
           </p>
         </div>
+
         <div className='flex space-x-3'>
           <button className='md:text-xl; flex cursor-pointer items-center gap-x-2 rounded bg-white px-5 py-1.5 text-sm font-semibold text-black transition hover:opacity-75 md:px-8 md:py-2.5'>
             Play
