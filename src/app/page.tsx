56f8803132a01434a40248f061ce93a36ae19cb1
Lucassamuel97 <lukassamuka88@gmail.com>
2025-07-23 20:36:21 -0300

    :sparkles: feat: Add @heroicons/react and tailwind-scrollbar-hide dependencies; update layout and page components for improved styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import { Inter } from 'next/font/google';
 import Header from './components/Header';
+import { InformationCircleIcon, PlayIcon } from '@heroicons/react/24/outline';
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
@@ -9,7 +10,7 @@ export default function Home() {
       <Header />
       <main className='relative pb-24 pl-4 lg:pl-16'>
         <div className='flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12'>
-          <div className='absolute left-0 top-0 -z-10 flex h-[95vh] w-screen flex-col bg-black'>
+          <div className='absolute top-0 left-0 -z-10 flex h-[95vh] w-screen flex-col bg-black'>
             <Image
               src='/banner.png'
               alt='MAID'
@@ -20,9 +21,7 @@ export default function Home() {
 
           <h1 className='text-2xl font-bold md:text-4xl lg:text-7xl'>MAID</h1>
 
-
-
-          <p className='text-shadow-md max-w-xs text-xs md:max-w-lg md:text-lg lg:max-w-2xl'>
+          <p className='max-w-xs text-xs text-shadow-md md:max-w-lg md:text-lg lg:max-w-2xl'>
             After fleeing an abusive relationship, a young mother finds a job
             cleaning houses as she fights to provide for her child and build
             them a better future.
@@ -31,10 +30,10 @@ export default function Home() {
 
         <div className='flex space-x-3'>
           <button className='md:text-xl; flex cursor-pointer items-center gap-x-2 rounded bg-white px-5 py-1.5 text-sm font-semibold text-black transition hover:opacity-75 md:px-8 md:py-2.5'>
-            Play
+            <PlayIcon className='h-6' />
           </button>
           <button className='md:text-xl; flex cursor-pointer items-center gap-x-2 rounded bg-gray-600 px-5 py-1.5 text-sm font-semibold text-black transition hover:opacity-75 md:px-8 md:py-2.5'>
-            More Info
+            <InformationCircleIcon className='h-6' />
           </button>
         </div>
       </main>
