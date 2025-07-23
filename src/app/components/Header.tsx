4164cd9aeb9ffc7130e74cc29d360e2b2c851bc7
Lucassamuel97 <lukassamuka88@gmail.com>
2025-07-23 15:37:24 -0300

    :recycle: refactor: Add logo and profile images; refactor Header component for improved structure and styling

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,53 +1,72 @@
 'use client';
+import Image from 'next/image';
 import React, { useEffect, useState } from 'react';
 
-export default function Header() {
+const useScroll = () => {
   const [isScrolled, setIsScrolled] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
-      if (window.scrollY > 0) {
-        setIsScrolled(true);
-      } else {
-        setIsScrolled(false);
-      }
+      setIsScrolled(window.scrollY > 0);
     };
 
     window.addEventListener('scroll', handleScroll);
 
+    // Cleanup remove o comportamento do scroll quando o componente é desmontado
     return () => {
       window.removeEventListener('scroll', handleScroll);
     };
   }, []);
 
+  return isScrolled;
+};
+
+const Logo = () => (
+  <Image
+    src='/logo.png'
+    alt='SamucaFlix'
+    width={90}
+    height={90}
+    className='cursor-pointer'
+  />
+);
+
+const NavLinks = () => (
+  <nav>
+    <ul className='hidden md:flex md:space-x-4'>
+      <li>Home</li>
+      <li>TV Shows</li>
+      <li>Movies</li>
+      <li>Latest</li>
+    </ul>
+  </nav>
+);
+
+const UserProfile = () => (
+  <div className='flex items-center space-x-4'>
+    <p className='hidden cursor-not-allowed lg:inline'>Kids</p>
+    <Image
+      src='/profile.png'
+      alt='profile picture'
+      width={30}
+      height={30}
+      className='cursor-pointer rounded'
+    />
+  </div>
+);
+
+export default function Header() {
+  const isScrolled = useScroll();
+
   return (
     <header
-      className={`
-    ${isScrolled && 'bg-black'}
-    fixed top-0 z-50 flex w-full items-center justify-between  px-4 py-4 transition-all lg:px-10 lg:py-6 `}
+      className={` ${isScrolled ? 'bg-black/70' : 'bg-transparent'} fixed top-0 z-50 flex w-full items-center justify-between px-4 py-4 transition-all lg:px-10 lg:py-6`}
     >
       <div className='flex items-center space-x-2 md:space-x-8'>
-        <img
-          src='https://raw.githubusercontent.com/Lucassamuel97/admin-videos-react/main/logo.png'
-          alt='netflix'
-          width={120}
-          height={120}
-        />
-        <ul className='hidden md:flex md:space-x-4'>
-          <li>Home</li>
-          <li>TV Shows</li>
-          <li>Movies</li>
-          <li>Latest</li>
-        </ul>
+        <Logo />
+        <NavLinks />
       </div>
-      <div className='flex items-center space-x-4'>
-        <p className='hidden cursor-not-allowed lg:inline'>Kids</p>
-        <img
-          src='https://rb.gy/g1pwyx'
-          alt=''
-          className='cursor-pointer rounded'
-        />
-      </div>
+      <UserProfile />
     </header>
   );
 }
