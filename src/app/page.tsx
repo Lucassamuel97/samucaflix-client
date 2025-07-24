22df56ea43e4553c520695a0f347fce0a1fa79d9
Lucassamuel97 <lukassamuka88@gmail.com>
2025-07-23 23:54:15 -0300

    :sparkles: feat: Implement Banner and MovieRow components; update layout for enhanced content display

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,18 @@
-import Image from 'next/image';
-import { Inter } from 'next/font/google';
+import { Banner } from './components/Banner';
 import Header from './components/Header';
-import { InformationCircleIcon, PlayIcon } from '@heroicons/react/24/outline';
-const inter = Inter({ subsets: ['latin'] });
+import { MovieRow } from './components/MovieRow';
 
 export default function Home() {
   return (
     <div className='relative h-screen overflow-hidden bg-gradient-to-b lg:h-[140vh]'>
       <Header />
       <main className='relative pb-24 pl-4 lg:pl-16'>
-        <div className='flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12'>
-          <div className='absolute top-0 left-0 -z-10 flex h-[95vh] w-screen flex-col bg-black'>
-            <Image
-              src='/banner.png'
-              alt='MAID'
-              fill={true}
-              className='h-[65vh] object-cover object-top lg:h-[95vh]'
-            />
-          </div>
-
-          <h1 className='text-2xl font-bold md:text-4xl lg:text-7xl'>MAID</h1>
-
-          <p className='max-w-xs text-xs text-shadow-md md:max-w-lg md:text-lg lg:max-w-2xl'>
-            After fleeing an abusive relationship, a young mother finds a job
-            cleaning houses as she fights to provide for her child and build
-            them a better future.
-          </p>
-        </div>
-
-        <div className='flex space-x-3'>
-          <button className='md:text-xl; flex cursor-pointer items-center gap-x-2 rounded bg-white px-5 py-1.5 text-sm font-semibold text-black transition hover:opacity-75 md:px-8 md:py-2.5'>
-            <PlayIcon className='h-6' />
-          </button>
-          <button className='md:text-xl; flex cursor-pointer items-center gap-x-2 rounded bg-gray-600 px-5 py-1.5 text-sm font-semibold text-black transition hover:opacity-75 md:px-8 md:py-2.5'>
-            <InformationCircleIcon className='h-6' />
-          </button>
-        </div>
+        <Banner />
+        <MovieRow sectionTitle='Featured' />
+        <MovieRow sectionTitle='Top Rated' />
+        <MovieRow sectionTitle='Trending' />
+        <MovieRow sectionTitle='Action' />
+        <MovieRow sectionTitle='Comedy' />
       </main>
     </div>
   );
