f6fd6a20cadf9461b6a486383d002cda8c764e2c
Lucassamuel97 <lukassamuka88@gmail.com>
2025-07-28 23:47:09 -0300

    :recycle: refactor: Refactor authentication forms to use AuthForm component and create InputField component

--- src/app/auth/login/Login.tsx
+++ src/app/auth/login/Login.tsx
@@ -1,53 +1,13 @@
 'use client';
 
 import React from 'react';
-import { InputField } from '../InputField';
+import { InputField } from '../../components/InputField';
+import { AuthForm } from '@/app/components/AuthForm';
 
 export default function LoginForm() {
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     console.log('submit');
     e.preventDefault();
   };
-  return (
-    <form
-      className='flex w-full max-w-md flex-col space-y-4 rounded bg-[#141414] px-4 py-8 opacity-90 shadow-lg'
-      onSubmit={handleSubmit}
-    >
-      <div className='flex flex-col items-center space-y-4'>
-        <h1 className='text-3xl font-bold'>Login</h1>
-        <p className='text-sm text-gray-500'>
-          New to the app?{' '}
-          <a href='#' className='text-red-500 hover:underline'>
-            Register
-          </a>
-        </p>
-      </div>
-      <div className='mt-8 flex flex-col space-y-4'>
-        <InputField
-          label='Email'
-          type='email'
-          id='email'
-          placeholder='Enter your email'
-          required
-          autoComplete='email'
-        />
-
-        <InputField
-          label='Password'
-          type='password'
-          id='password'
-          placeholder='Enter your password'
-          required
-          autoComplete='current-password'
-        />
-      </div>
-
-      <button
-        type='submit'
-        className='rounded bg-red-600 px-4 py-2 font-bold text-white hover:bg-red-700'
-      >
-        Login
-      </button>
-    </form>
-  );
+  return <AuthForm formType='login' onSubmit={handleSubmit} />;
 }
--- src/app/auth/register/RegisterForm.tsx
+++ src/app/auth/register/RegisterForm.tsx
@@ -1,7 +1,8 @@
 'use client';
 
 import React from 'react';
-import { InputField } from '../InputField';
+import { InputField } from '../../components/InputField';
+import { AuthForm } from '@/app/components/AuthForm';
 
 export default function RegisterForm() {
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
@@ -9,64 +10,5 @@ export default function RegisterForm() {
     e.preventDefault();
   };
 
-  return (
-    <form
-      className='flex w-full max-w-md flex-col space-y-4 rounded bg-[#141414] px-4 py-8 opacity-90 shadow-lg'
-      onSubmit={handleSubmit}
-    >
-      <div className='flex flex-col items-center space-y-4'>
-        <h1 className='text-3xl font-bold'>Register</h1>
-        <p className='text-sm text-gray-500'>
-          Already have an account?{' '}
-          <a href='#' className='text-red-500 hover:underline'>
-            Login
-          </a>
-        </p>
-      </div>
-      <div className='mt-8 flex flex-col space-y-4'>
-        <InputField
-          label='Name'
-          type='text'
-          id='name'
-          placeholder='Enter your full name'
-          required
-          autoComplete='name'
-        />
-
-        <InputField
-          label='Email'
-          type='email'
-          id='email'
-          placeholder='Enter your email'
-          required
-          autoComplete='email'
-        />
-
-        <InputField
-          label='Password'
-          type='password'
-          id='password'
-          placeholder='Enter your password'
-          required
-          autoComplete='new-password'
-        />
-
-        <InputField
-          label='Confirm Password'
-          type='password'
-          id='confirmPassword'
-          placeholder='Confirm your password'
-          required
-          autoComplete='new-password'
-        />
-      </div>
-
-      <button
-        type='submit'
-        className='rounded bg-red-600 px-4 py-2 font-bold text-white hover:bg-red-700'
-      >
-        Register
-      </button>
-    </form>
-  );
+  return <AuthForm formType='register' onSubmit={handleSubmit} />;
 }
