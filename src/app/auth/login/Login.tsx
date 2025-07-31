8a9837cc163753a6764698e83cf908cfb6fdbe7f
Lucassamuel97 <lukassamuka88@gmail.com>
2025-07-30 21:34:11 -0300

    :sparkles: feat: Enhance LoginForm with error handling and API integration

--- src/app/auth/login/Login.tsx
+++ src/app/auth/login/Login.tsx
@@ -3,11 +3,50 @@
 import React from 'react';
 import { InputField } from '../../components/InputField';
 import { AuthForm } from '@/app/components/AuthForm';
+import { useRouter } from 'next/navigation';
+
+type ServerError = {
+  message: string;
+};
 
 export default function LoginForm() {
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    console.log('submit');
+
+  const router = useRouter();
+  const [errors, setErrors] = React.useState<string[]>([]);
+
+  console.log(errors);
+
+  const handleSubmit = async(e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const email = formData.get('email') as string;
+    const password = formData.get('password') as string;
+
+    try{
+      const response = await fetch('/auth/login/api', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log('Login successful:', data);
+
+        router.push('/');
+        return;
+      } 
+
+      const payload: ServerError[] = await response.json();
+
+      setErrors(payload.map((error) => error.message));
+
+    } catch (error) {
+      console.error('Login error:', error);
+      setErrors(['Login failed. Please try again.']);
+    }
   };
   return <AuthForm formType='login' onSubmit={handleSubmit} />;
 }
