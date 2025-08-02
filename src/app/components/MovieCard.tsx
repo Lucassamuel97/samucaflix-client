e12ef3bdb0e61edcdd72e897b9cf3fb19aea8fde
Lucassamuel97 <lukassamuka88@gmail.com>
2025-08-01 23:02:53 -0300

    :sparkles: feat: add MovieInfo and MovieRating components; update MovieCard to display movie details

--- src/app/components/MovieCard.tsx
+++ src/app/components/MovieCard.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import { Movie } from '../types /movie';
+import { MovieInfo } from './MovieInfo';
 
 export const MovieCard = ({ movie }: { movie: Movie }) => (
     <div className="group relative min-h-[12vh] rounded bg-zinc-900 md:min-h-[12vw]">
@@ -19,6 +20,8 @@ export const MovieCard = ({ movie }: { movie: Movie }) => (
                 height={400}
                 className="h-[12vw] w-full cursor-pointer rounded-t-md object-cover object-top shadow-xl transition-all"
             />
+
+            <MovieInfo movie={movie} />
         </div>
     </div>
 );
