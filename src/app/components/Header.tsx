50a0d05bc5084f7713959413a8ee35ac2c58b946
Lucassamuel97 <lukassamuka88@gmail.com>
2025-08-02 00:00:58 -0300

    :sparkles: feat: add search functionality to the header; create SearchForm component for managing search

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,13 +1,29 @@
 'use client';
-import React from 'react';
+import React, { useState } from 'react';
 import { Logo } from './Logo';
 import { NavLinks } from './NavLinks';
 import { UserProfile } from './UserProfile';
 import { useScroll } from '../hooks/useScroll';
+import { SearchForm } from './SearchForm';
+import { useSearchParams, useRouter } from 'next/navigation';
 
 export default function Header() {
   const isScrolled = useScroll();
+  const params = useSearchParams();
+  const router = useRouter();
+  const initialSearchTerm = params.get('title') || '';
+  const [searchTerm, setSearchTerm] = useState<string>(initialSearchTerm);
 
+  const onSearchTermChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const onSearch = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const newParams = new URLSearchParams(params.toString());
+    newParams.set('title', searchTerm);
+    router.push(`/search?${newParams.toString()}`);
+  };
   return (
     <header
       className={`${isScrolled ? 'bg-black/70' : 'bg-transparent'} fixed top-0 z-50 flex w-full items-center justify-between bg-gradient-to-t from-transparent to-black p-2 px-4 transition-all lg:px-16 lg:py-4`}
@@ -16,7 +32,14 @@ export default function Header() {
         <Logo />
         <NavLinks />
       </div>
-      <UserProfile />
+      <div className='flex items-center space-x-2 md:space-x-8'>
+        <SearchForm
+          onSearch={onSearch}
+          searchTerm={searchTerm}
+          onSearchTermChange={onSearchTermChange}
+        />
+        <UserProfile />
+      </div>
     </header>
   );
 }
